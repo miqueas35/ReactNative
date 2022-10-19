fd122cbf9913d9c869aeab7e9d6cf5cbf2a34964
Marcelo Acevedo <miqueasacevedo2017@gmail.com>
2022-10-19 14:40:41 -0300

    init react native project

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image, TouchableHighlight, Text} from 'react-native';
+import { Title } from './src/components/Title';
 
-export default function App() {
+const App = () => {
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Title title='React Avanzado'/>
+      <TouchableHighlight
+        activeOpacity={0.6}
+        underlayColor="#DDDDDD"
+        onPress={() => alert('Pressed!')}>
+      <Text>Touch Here</Text>  
+      </TouchableHighlight>
       <StatusBar style="auto" />
+      <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={styles.image}
+        />
     </View>
   );
 }
@@ -17,4 +31,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: { 
+    width: 300, 
+    height: 300 
+  }
 });
+
+export default App;
---
+++ src/components/Title/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const Title = ({title}) => {
+  return (
+    <Text>
+      {title}
+    </Text>
+  )
+}
+
+export default Title
