b52ebcde52b87b4bbdbb76163b5c006ef79cf542
Marcelo Acevedo <miqueasacevedo2017@gmail.com>
2022-10-21 13:25:32 -0300

    add card component

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, View, Text, Image} from 'react-native';
+import { StyleSheet, View,} from 'react-native';
+import { Card } from './src/components/Card';
 import { Title } from './src/components/Title';
 
 const App = () => {
@@ -21,47 +22,20 @@ const App = () => {
       <Title title='React Avanzado'/>
       {
         products.map(product => (
-      <View style={styles.card}>
-      <Text style={styles.titleCard}>{product.name}</Text>
-      <Image source={{ uri: product.image }} style={styles.image}/>
-      <Text style={styles.priceCard}>Precio: ${product.id * 1003}</Text>
-      </View>
+          <View key={product.id}>
+            <Card name={product.name} uri={product.image} price={product.id}/>
+          </View>
         ))
       }
-      
-    </View>
+    </View> 
   );
-}
+} 
+  
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
     padding: 20
-  },
-  card: {
-    width: '100%',
-    height: 350,
-    border: '1px solid black',
-    marginBottom: 20
-  },
-  titleCard: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginHorizontal: 20,
-    marginVertical: 15,
-    textAlign: 'center'
-  },
-  image: {
-    height: '70%',
-    maxWidth: '100%',
-    maxHeight: '70%',
-    marginHorizontal: 15
-  },
-  priceCard: {
-    fontSize: 15,
-    textAlign: 'center',
-    fontWeight: 900,
-    marginTop: 15
   }
 });
 
---
+++ src/components/Card/Card.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { StyleSheet, View, Text, Image} from 'react-native'
+
+const Card = (props) => {
+    return(
+        <View style={styles.card}>
+            <Text style={styles.titleCard}>{props.name}</Text>
+            <Image source={{ uri: props.image }} style={props.image}/>
+            <Text style={styles.priceCard}>Precio: ${props.price * 1003}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+    card: {
+        width: '100%',
+        height: 350,
+        border: '1px solid black',
+        marginBottom: 20
+      },
+      titleCard: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginHorizontal: 20,
+        marginVertical: 15,
+        textAlign: 'center'
+      },
+      image: {
+        height: '70%',
+        maxWidth: '100%',
+        maxHeight: '70%',
+        marginHorizontal: 15
+      },
+      priceCard: {
+        fontSize: 15,
+        textAlign: 'center',
+        fontWeight: '900',
+        marginTop: 15
+      }
+  })
+
+export default Card
